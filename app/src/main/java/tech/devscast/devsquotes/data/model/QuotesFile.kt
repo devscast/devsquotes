d74923d8ef3ccf50cef39e5010723cbcf27e75f4
YvesKalume <yvkalume@gmail.com>
2022-09-05 17:15:37 +0200

    fix retrofit release

--- app/src/main/java/tech/devscast/devsquotes/data/model/QuotesFile.kt
+++ app/src/main/java/tech/devscast/devsquotes/data/model/QuotesFile.kt
@@ -1,15 +1,29 @@
 package tech.devscast.devsquotes.data.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class QuotesFile(
+    @SerializedName("download_url")
     val download_url: String,
+
+    @SerializedName("encoding")
     val encoding: String,
+
+    @SerializedName("git_url")
     val git_url: String,
+
+    @SerializedName("name")
     val name: String,
+
+    @SerializedName("path")
     val path: String,
+
+    @SerializedName("size")
     val size: Int,
+
+    @SerializedName("url")
     val url: String
 ) : Parcelable
